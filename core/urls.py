02269a909120b1679df2598cd031c039214ed55d
Yonas Abebe <yonasa278@gmail.com>
2025-08-09 00:20:53 +0300

    .

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('mlapi.urls')), 
-    path('api/v1/', include('coffee.urls')),
+   
      
     
 ]
+
